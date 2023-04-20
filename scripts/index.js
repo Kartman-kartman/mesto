994392585a5b1b8949b5d40d4d47e3f815722d79
Dmitrii Puhov <mankartman@yandex.ru>
2023-04-20 18:35:47 +0300

    go4

--- scripts/index.js
+++ scripts/index.js
@@ -4,44 +4,33 @@ const aboutButton = document.querySelector('.profile__button')
 const popup = document.querySelector('.popup')
 const popupoff = popup.querySelector('.popup__close')
 const popupsave = popup.querySelector('.popup__save')
+let title = document.querySelector('.profile__title')
+let subtitle = document.querySelector('.profile__subtitle')
+let formElement = document.querySelector('.popup__form')
+let input = document.querySelector('.popup__input_name')
+let inputsecond = document.querySelector('.popup__input_job')
 
 const ButtonClick = () => {
     popup.classList.add('popup_opened');
+    input.defaultValue = title.textContent;
+    inputsecond.defaultValue = subtitle.textContent;
 }
+
+
 const CloseButtonClick = () => {
     popup.classList.remove('popup_opened');
 }
 
-
 aboutButton.addEventListener('click', ButtonClick)
 popupoff.addEventListener('click', CloseButtonClick)
-popupsave.addEventListener('click', CloseButtonClick)
-
-
-
-let formElement = document.querySelector('.popup__form')
-let input = document.querySelector('.popup__inputname')
-let inputsecond = document.querySelector('.popup__inputjob')
-
-
-let title = document.querySelector('.profile__title')
-let subtitle = document.querySelector('.profile__subtitle')
-input.defaultValue = title.textContent;
-inputsecond.defaultValue = subtitle.textContent;
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
     title.textContent = input.value;
     subtitle.textContent = inputsecond.value;
+    CloseButtonClick('onClick')
 }
 formElement.addEventListener('submit', handleFormSubmit);
-
-const last = document.querySelector('#lasttext')
-const screenWidth = window.screen.width
-if (screenWidth <= 371) {
-    last.textContent = "Карачаевск";
-}
-
 
 
 
