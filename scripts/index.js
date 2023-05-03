265320d4ea2931d5d80f6d4fad73eb00494ff3b3
Dmitrii Puhov <mankartman@yandex.ru>
2023-05-03 10:58:27 +0300

    go4

--- scripts/index.js
+++ scripts/index.js
@@ -7,28 +7,28 @@ const popupsave = popup.querySelector('.popup__save')
 let title = document.querySelector('.profile__title')
 let subtitle = document.querySelector('.profile__subtitle')
 let formElement = document.querySelector('.popup__form')
-let input = document.querySelector('.popup__input_name')
-let inputsecond = document.querySelector('.popup__input_job')
+let input = document.querySelector('.popup__input_text_name')
+let inputsecond = document.querySelector('.popup__input_text_job')
 
-const ButtonClick = () => {
+const buttonClick = () => {
     popup.classList.add('popup_opened');
     input.defaultValue = title.textContent;
     inputsecond.defaultValue = subtitle.textContent;
 }
 
 
-const CloseButtonClick = () => {
+const closeButtonClick = () => {
     popup.classList.remove('popup_opened');
 }
 
-aboutButton.addEventListener('click', ButtonClick)
-popupoff.addEventListener('click', CloseButtonClick)
+aboutButton.addEventListener('click', buttonClick)
+popupoff.addEventListener('click', closeButtonClick)
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
     title.textContent = input.value;
     subtitle.textContent = inputsecond.value;
-    CloseButtonClick('onClick')
+    closeButtonClick('onClick')
 }
 formElement.addEventListener('submit', handleFormSubmit);
 
